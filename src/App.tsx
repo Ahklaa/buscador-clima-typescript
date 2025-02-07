3de74eace52c14b8530484cc70af99444670adfd
Ahkla <ahkla185@hotmail.com>
2025-02-07 14:41:58 -0500

    se inicializo el proyecto, ademas se agregaron estilos modulos

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import style from './App.module.css'
+function App() {
+  return (
+    <>
+     <h1 className={style.title}>Buscador de clima</h1>
+     <div className={style.container}>
+      <p>1</p>
+      <p>2</p>
+     </div>
+    </>
+  )
+}
+
+export default App
