66ecb036a7089cbe51e1d095ddaa0bc35f2181d0
Ahkla <ahkla185@hotmail.com>
2025-02-07 15:59:35 -0500

    se creo el estado de la busqueda y se crearon dos types para los countries traidos de db y el de search

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,20 @@
+import { ChangeEvent, useState } from "react"
 import { countries } from "../../db/countries"
 import style from './Form.module.css'
+import { Search } from "../../types"
+
 export default function Form() {
+
+  const [search,setSearch] = useState<Search>({
+    city : "",
+    country : ""
+  })
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    setSearch({
+      ...search,
+      [e.target.name] : e.target.value
+    })
+  }
   return (
     <form className={style.form}>
         <div className={style.field}>
@@ -9,11 +23,13 @@ export default function Form() {
             type="text" 
             name="city"
             id="city"  
-            placeholder="Escriba la ciudad"/>
+            placeholder="Escriba la ciudad"
+            onChange={handleChange}/>
+            value = {search.city}
         </div>
         <div className={style.field}>
          <label htmlFor="country">País</label>
-           <select name="country" id="country">
+           <select name="country" id="country" value={search.country} onChange={handleChange}>
             <option value="">--Seleccione el pais</option>
             {countries.map(country => (
                 <option value={country.code} key={country.code}>{country.name}</option>
