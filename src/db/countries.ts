7858daba7d536bf131fd9948a361d6b367b1c25e
Ahkla <ahkla185@hotmail.com>
2025-02-10 16:33:40 -0500

    se agrego el pais de brasil

--- src/db/countries.ts
+++ src/db/countries.ts
@@ -6,5 +6,6 @@ export const countries : Country[] = [
     { code: 'CO', name: 'Colombia' },
     { code: 'CR', name: 'Costa Rica' },
     { code: 'ES', name: 'España' },
-    { code: 'PE', name: 'Perú' }
+    { code: 'PE', name: 'Perú' },
+    { code: "BR", name: "Brasil"}
   ]
