083e771061cb172bc81edf5bf89ad43c71fdefa2
Ahkla <ahkla185@hotmail.com>
2025-02-10 14:37:46 -0500

    se hizo el tipado de la api con valibot y se comentaron las otras maneras de tipado para futuros proyectos

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,10 +1,10 @@
-import { Search } from "../types"
-import axios from "axios" 
-import {z} from 'zod'
+import { Search } from "../types";
+import axios from "axios";
+import { string, number, object, parse, InferOutput } from "valibot";
+//import {z} from 'zod'
 //TYPE GUARD AND ASSERTION
 export default function useWeather() {
   /*  const isWeatherType = (weather : unknown) : weather is Weather => {
-
         return (
            Boolean(weather) && 
            typeof weather === "object" &&
@@ -16,8 +16,8 @@ export default function useWeather() {
    }
     */
 
-   //ZOD
-    const Weather = z.object({
+  //ZOD
+  /* const Weather = z.object({
         name : z.string(),
         main : z.object({
             temp: z.number(),
@@ -25,22 +25,41 @@ export default function useWeather() {
             temp_min : z.number()
         })
     })
-
     type Weather = z.infer<typeof Weather>
+    */
 
-    const fetchWeather = async (search : Search) => {
-       try {
-            const apiKeyWeather =  import.meta.env.VITE_API_KEY
-            
-            const urlGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKeyWeather}`
-            
-            const {data} = await axios.get(urlGeo)
+  //VALIBOT
+  const WeatherSchema = object({
+    name: string(),
+    main: object({
+      temp: number(),
+      temp_max: number(),
+      temp_min: number(),
+    }),
+  });
 
-            const lat  = data[0].lat
-            const lon = data[0].lon
-            const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeyWeather}`
-            const {data:weatherData} = await  axios.get(urlWeather)
-           /* const result = isWeatherType(weatherData)
+  type Weather = InferOutput<typeof WeatherSchema>;
+
+  const fetchWeather = async (search: Search) => {
+    try {
+      const apiKeyWeather = import.meta.env.VITE_API_KEY;
+
+      const urlGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKeyWeather}`;
+
+      const { data } = await axios.get(urlGeo);
+
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+      const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeyWeather}`;
+      const { data: weatherData } = await axios.get(urlWeather);
+      const result = parse(WeatherSchema, weatherData);
+      if (result) {
+        console.log(result.name);
+      } else {
+        console.log("No se pudo hacer el tipado para  API");
+      }
+
+      /* const result = isWeatherType(weatherData)
 
             if (result){
                 console.log(result);
@@ -49,24 +68,23 @@ export default function useWeather() {
                 console.log(result);
                 
             }       
-            */     
-            
-            //zod
-            const result = Weather.safeParse(weatherData)
+            */
+
+      //zod
+      /*const result = Weather.safeParse(weatherData)
             if(result.success){
                 console.log(result.data);
                 
             }else {
                 console.log("No se pudo obtener datos");
                 
-            }
-       } catch (error) {
-            console.log(error);
-       }
-        
+            }*/
+    } catch (error) {
+      console.log(error);
     }
-    
-    return {
-        fetchWeather
-    }
+  };
+
+  return {
+    fetchWeather,
+  };
 }
