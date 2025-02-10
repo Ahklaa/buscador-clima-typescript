507c7bf142498c406d3f4201ac3ac9bdcbfec50f
Ahkla <ahkla185@hotmail.com>
2025-02-10 10:14:43 -0500

    se relaizo el segundo llamado al api

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -6,10 +6,17 @@ export default function useWeather() {
        try {
             const apiKeyWeather =  import.meta.env.VITE_API_KEY
             
-            const urlCurrentData = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKeyWeather}`
+            const urlGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKeyWeather}`
             
-            const {data} = await axios.get(urlCurrentData)
-            console.log(data);
+            const {data} = await axios.get(urlGeo)
+
+            const lat  = data[0].lat
+            const lon = data[0].lon
+            const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeyWeather}`
+            const {data:weatherData} = await  axios.get(urlWeather)
+            console.log(weatherData);
+            
+            
        } catch (error) {
             console.log(error);
        }
