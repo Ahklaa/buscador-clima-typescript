2fae26c07c12b95066420d3dcbc00558a5d4aeb9
Ahkla <ahkla185@hotmail.com>
2025-02-10 13:20:46 -0500

    realizando el tipado de los datos de la api usando Type Guard

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,8 +1,22 @@
 import { Search } from "../types"
 import axios from "axios" 
+import type { Weather } from "../types"
+
+//TYPE GUARD AND ASSERTION
 export default function useWeather() {
+    const isWeatherType = (weather : unknown) : weather is Weather => {
+
+        return (
+           Boolean(weather) && 
+           typeof weather === "object" &&
+           typeof (weather as Weather).name === "string" &&
+           typeof (weather as Weather).main.temp === "number" &&
+           typeof (weather as Weather).main.temp_max === "number" &&
+           typeof (weather as Weather).main.temp_min === "number"
+        )
+   }
+
     const fetchWeather = async (search : Search) => {
-        
        try {
             const apiKeyWeather =  import.meta.env.VITE_API_KEY
             
@@ -14,8 +28,15 @@ export default function useWeather() {
             const lon = data[0].lon
             const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeyWeather}`
             const {data:weatherData} = await  axios.get(urlWeather)
-            console.log(weatherData);
-            
+            const result = isWeatherType(weatherData)
+
+            if (result){
+                console.log(result);
+                
+            }else{
+                console.log(result);
+                
+            }            
             
        } catch (error) {
             console.log(error);
